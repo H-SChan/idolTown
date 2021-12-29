d746b4af2396d37183f91787f7327783356e32cb
H-SChan <dlwp7581@gmail.com>
2021-12-29 09:49:38 +0900

    init

---
+++ src/main/java/com/example/idoltown/IdolTownApplication.java
@@ -0,0 +1,13 @@
+package com.example.idoltown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdolTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdolTownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/idoltown/IdolTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idoltown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdolTownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
