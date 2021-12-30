3f62dbdea285645c9b041f0bf949adbae7b93bf7
H-SChan <dlwp7581@gmail.com>
2021-12-30 10:17:23 +0900

    really real done

--- src/main/java/com/example/idoltown/Main.java
+++ src/main/java/com/example/idoltown/Main.java
@@ -3,10 +3,12 @@ package com.example.idoltown;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.ColumnDefault;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +16,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -23,19 +26,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 class MainController {
 
     private final Repo repo;
 
-    private final RestTemplate restTemplate = new RestTemplate();
     private final JSONParser jsonParser = new JSONParser();
 
     @GetMapping("/display/{fileName}")
@@ -57,16 +63,23 @@ class MainController {
 
     @GetMapping("/ranking")
     public List<Ranking> getRanking() {
+        log.info("ranking");
         return repo.findAllByImgUrlOrderByPoint().orElseGet(() -> null);
     }
 
+    @Getter @Setter
+    static class Winner {
+        private String who;
+    }
+
     @Transactional
-    @PatchMapping("/win")
-    public String win(@RequestBody String who) {
-        Ranking winner = repo.findByName(who).orElseThrow(
+    @PostMapping("/win")
+    public String win(@RequestBody Winner winner) {
+        log.info("winner -> " + winner.getWho());
+        Ranking who = repo.findByName(winner.getWho()).orElseThrow(
                 () -> new IllegalArgumentException("없는놈")
         );
-        winner.setPoint(winner.getPoint() + 1);
+        who.setPoint(who.getPoint() + 1);
         return "성공";
     }
 
@@ -115,4 +128,23 @@ interface Repo extends JpaRepository<Ranking, Long> {
     Optional<List<Ranking>> findAllByImgUrlOrderByPoint();
 
     Optional<Ranking> findByName(String name);
+}
+
+@Order(1)
+@Component
+class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH, HEAD");
+        res.setHeader("Access-Control-Max-Age", "-1");
+        res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers, Cache-Control, Pragma, Expires");
+        res.setHeader("Access-Control-Expose-Headers", "content-disposition");
+
+        chain.doFilter(request, response);
+    }
+
 }
